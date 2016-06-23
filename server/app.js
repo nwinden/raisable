d10b7eceec71b14b75d2a44ca441cfada98c4891
nwinden <noah.winden@gmail.com>
2016-06-23 14:05:51 -0500

    First Commit

---
+++ server/app.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+
+//middleware
+app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// mongoose connection
+var databaseURI = 'mongodb://localhost:27017/raisable';
+
+mongoose.connect(databaseURI);
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connection open ', databaseURI);
+});
+
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose error connecting ', err);
+});
+
+// start server
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function () {
+  console.log('listening on port ', app.get('port'));
+});
+
+// Handle index file separately
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/views/index.html'));
+});
---
+++ server/public/scripts/clientApp.js
@@ -0,0 +1,5 @@
+var clientApp = angular.module('clientApp', []);
+
+clientApp.config([function() {
+  console.log('Hey Buckaroo!');
+}]);
