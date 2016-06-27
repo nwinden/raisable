d122a9556900f9d3be392c7ac6584f08d9f41710
Zach Kusz <zskusz@gmail.com>
2016-06-27 11:25:33 -0500

    basic percent progress bar

--- server/public/scripts/landingController.js
+++ server/public/scripts/landingController.js
@@ -182,7 +182,16 @@ clientApp.controller('LandingController', ['$scope','$location', function($scope
     "pinterestShareText": "help the slp spartans get new uniforms raisable.com/lksdfj3c",
   }; //end of object
 
-
+  var bar = new ProgressBar.Line(progressLine, {
+    strokeWidth: 4,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: 'blue',
+    trailColor: '#eee',
+    trailWidth: 1,
+    svgStyle: {width: '100%', height: '100%'}
+  });
+  bar.animate(1.0);  // Number from 0.0 to 1.0
 
 
 
